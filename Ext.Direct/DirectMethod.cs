97214bb972018840a86014a59b8a2b5743b68d3c
westy <martyn.west+gibthub@gmail.com>
2011-03-14 11:08:48 +0000

    Updated namespace for attributes. Removed commented out ParseAsJson code.

--- Ext.Direct/DirectMethod.cs
+++ Ext.Direct/DirectMethod.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+
+using Ext.Direct.Attributes;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -20,7 +23,6 @@ namespace Ext.Direct
             this.Method = method;
             this.IsForm = Utility.HasAttribute(method, typeof(DirectMethodFormAttribute));
             this.Name = Utility.GetName(method);
-            //this.ParseAsJson = Utility.HasAttribute(method, typeof(ParseAsJsonAttribute));
             this.Parameters = method.GetParameters().Length;
         }
 
@@ -59,40 +61,6 @@ namespace Ext.Direct
             get;
             private set;
         }
-
-        //internal bool ParseAsJson
-        //{
-        //    get;
-        //    private set;
-        //}
-
-        //internal JContainer GetParseData(JObject requestData)
-        //{
-        //    object[] attrs = this.Method.GetCustomAttributes(typeof(ParseAsJsonAttribute), true);
-        //    if (attrs.Length > 0)
-        //    {
-        //        ParseAsJsonAttribute attr = attrs[0] as ParseAsJsonAttribute;
-        //        if (attr != null)
-        //        {
-        //            try
-        //            {
-        //                JArray data = (JArray) requestData["data"];
-        //                if (attr.AsArray)
-        //                {
-        //                    return (JArray)data[0];
-        //                }
-        //                else
-        //                {
-        //                    return (JObject)data[0];
-        //                }
-        //            }
-        //            catch (Exception ex)
-        //            {
-        //            }
-        //        }
-        //    }
-        //    return null;
-        //}
 
         /// <summary>
         /// Write API JSON.
