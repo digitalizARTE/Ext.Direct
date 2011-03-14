e452eabb90abd143d7cbdd0ca4500b5cceab9f2b
westy <martyn.west+gibthub@gmail.com>
2011-03-14 11:06:25 +0000

    Updated to include all Exception constructors, for completeness.

--- Ext.Direct/DirectException.cs
+++ Ext.Direct/DirectException.cs
@@ -1,30 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.Serialization;
 
 namespace Ext.Direct
 {
-
     /// <summary>
     /// Thrown when an error occurs during a Ext.Direct call.
     /// </summary>
     public class DirectException : ApplicationException
     {
-
+        #region Constructors
+        /// <summary>
+        /// Initialises an instance of this class
+        /// </summary>
         public DirectException()
             : base()
         {
         }
 
-        public DirectException(string msg)
-            : base(msg)
+        /// <summary>
+        /// Initialises an instance of this class
+        /// </summary>
+        /// <param name="message">The message that describes the error</param>
+        public DirectException(string message)
+            : base(message)
         {
         }
 
-        public DirectException(string msg, Exception innerException)
-            : base(msg, innerException)
+        /// <summary>
+        /// Initialises an instance of this class
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference
+        /// if no inner exception is specified</param>
+        public DirectException(string message,
+                               System.Exception innerException)
+            : base(message,
+                   innerException)
         {
         }
+
+        /// <summary>
+        /// Initialises an instance of this class
+        /// </summary>
+        /// <param name="info">
+        /// The System.Runtime.Serialization.SerializationInfo that holds the serialized
+        /// object data about the exception being thrown.
+        /// </param>
+        /// <param name="context">
+        /// The System.Runtime.Serialization.StreamingContext that contains contextual
+        /// information about the source or destination.
+        /// </param>
+        protected DirectException(SerializationInfo info,
+                                  StreamingContext  context)
+            : base(info,
+                   context)
+        {
+        }
+        #endregion
     }
 }
